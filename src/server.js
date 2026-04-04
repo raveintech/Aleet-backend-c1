53b6ce892719fcfb6e51b684c57da211495b28fb
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2026-04-04 17:56:14 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ connectDB();
 
 const app = express();
 
+app.get('/health', (req, res) => res.status(200).json({ status: 'Aleet Backend is running' }));
+
 // Import routes
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
